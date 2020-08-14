2b3ba02a1c547231c149544bfb1f759edb6abf03
Gwanghyeon Gim <harryghgim@gmail.com>
2020-08-14 13:40:31 +0900

    Updates

---
+++ precache-manifest.679e8b40cf7688fdaf02c2bf35217cff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74383d26fbdee6e21bed734cc69d93b5",
+    "url": "/mdn-todo-react/index.html"
+  },
+  {
+    "revision": "bae9852ea8263fcc3331",
+    "url": "/mdn-todo-react/static/css/main.220fcbac.chunk.css"
+  },
+  {
+    "revision": "9e3cdfe2c7343a015e9a",
+    "url": "/mdn-todo-react/static/js/2.0c51fe6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mdn-todo-react/static/js/2.0c51fe6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bae9852ea8263fcc3331",
+    "url": "/mdn-todo-react/static/js/main.a4dfef68.chunk.js"
+  },
+  {
+    "revision": "93ef469968c28880b3c1",
+    "url": "/mdn-todo-react/static/js/runtime-main.ff400250.js"
+  }
+]);
